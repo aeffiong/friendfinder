3016d9e3b89c03ae6cfb53d3ba35a280d545e70b
aeffiong <aeffiong9@gmail.com>
2018-07-17 16:20:11 -0600

    getting api routes up

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,3 +2,14 @@
 var friendData = require("./app/data/friends");
 
 // routing
+module.exports = function(app) {
+    // GET request
+    app.get("/api/friends", function(req, res) {
+        res.json(friendData);
+      });
+    // POST request
+    app.post("/api/friends", function(req, res) {
+        friendData.push(req.body);
+        // compatibility logic
+    })
+}
