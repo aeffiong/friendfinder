528767e3a7a1433722d42311d04b092d02a8c68c
aeffiong <aeffiong9@gmail.com>
2018-07-18 19:49:09 -0600

    working on compatibility logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,7 +9,26 @@ module.exports = function(app) {
       });
     // POST request
     app.post("/api/friends", function(req, res) {
-        friendData.push(req.body);
         // compatibility logic
+        var userInfo = req.body;
+        console.log(userInfo);
+
+        var userScores = req.body.scores;
+        // console.log(userScores);
+
+        // check all existing friends in the list
+        for (var i = 0; i < friendData.length; i++) {
+            console.log(friendData[i]);
+
+            // declare diff variable to show the difference in scores
+            var diff = 0;
+            // Looping through the scores - seems to only be doing it on the first index
+            for (var j = 0; j < userScores.length; j++) {
+                diff = Math.abs(friendData[i].scores[j] - parseInt(userScores[j]));
+            }
+            console.log(diff);
+        }
+
+
     })
 }
