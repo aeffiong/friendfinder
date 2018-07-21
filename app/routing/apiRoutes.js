80a2cf8c3737835da06bbaaa287cc35a0e4a53eb
aeffiong <aeffiong9@gmail.com>
2018-07-20 20:52:21 -0600

    got match and modal to work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,24 +10,41 @@ module.exports = function(app) {
     // POST request
     app.post("/api/friends", function(req, res) {
         // compatibility logic
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDiff: 100
+        };
+
         var userInfo = req.body;
         console.log(userInfo);
 
         var userScores = req.body.scores;
         // console.log(userScores);
 
+        var diff = 0;
+
+
         // check all existing friends in the list
         for (var i = 0; i < friendData.length; i++) {
             console.log(friendData[i]);
-
-            // declare diff variable to show the difference in scores
             var diff = 0;
-            // Looping through the scores - seems to only be doing it on the first index
+            // Looping through the scores
             for (var j = 0; j < userScores.length; j++) {
-                diff = Math.abs(friendData[i].scores[j] - parseInt(userScores[j]));
+                diff += Math.abs(parseInt(friendData[i].scores[j]) - parseInt(userScores[j]));
             }
             console.log(diff);
+            
+            if (diff <= bestMatch.friendDiff) {
+                bestMatch.name = friendData[i].name;
+                bestMatch.photo = friendData[i].photo;
+                bestMatch.friendDiff = diff;
+                
+            }
+            
         }
+        friendData.push(userInfo);
+        res.json(bestMatch);
 
 
     })
